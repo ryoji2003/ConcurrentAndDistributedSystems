0b64ae7452fbd9e60fbebc6d8008d10ef724af09
Ryoji Hagihara <ryoji@RyojinoMacBook-Pro.local>
2026-01-26 05:04:42 +0900

    upload ex9 files

--- ex/ex9/w09_socketChat.java
+++ ex/ex9/w09_socketChat.java
@@ -2,128 +2,94 @@ import java.net.*;
 import java.io.*;
 import java.util.Vector;
 
-public class w09_socketChat
-{
-	static final int port = 12345;
-	
-	static BufferedReader getReader(InputStream is)
-	{
-		return new BufferedReader(new InputStreamReader(is));
-	}
-	
-	static BufferedWriter getWriter(OutputStream os)
-	{
-		return new BufferedWriter(new OutputStreamWriter(os));
-	}
+public class  w09_socketChat{
+    static final int port = 12345;
 
-	static void writeString(String str, BufferedWriter out) throws Exception
-	{
-		out.write(str, 0, str.length());
-		out.newLine();
-		out.flush();
-	}
+    static class AllSockets {
+        static private Vector<Socket> allSockets = new Vector<Socket>();
 
-	static class AllSockets
-	{
-		static private Vector<Socket> allSockets = new Vector<Socket>();
-
-		synchronized static public void Add(Socket client) { allSockets.add(client); }
-		
-		synchronized static public void Broadcast(Socket from, String str) throws Exception
-		{
-			for(Socket s : allSockets)	// send str to everybody except from
-				if(s != from)
-					writeString(str, getWriter(s.getOutputStream()));
-		}
-	}
-	
-	static class EchoIncoming implements Runnable
-    {
-		BufferedReader reader;
-
-        public EchoIncoming(BufferedReader r) { reader = r; } 
-
-        public void run()
-        {
-			try
-			{
-				for(;;)
-					System.out.println(reader.readLine());
-			}
-			catch(Exception e) {}
-		}
+        synchronized static public void Add(Socket client) { allSockets.add(client); }
+        
+        synchronized static public void Remove(Socket client) {
+            allSockets.remove(client);
+            try { client.close(); } catch (IOException e) { }
+        }
+        
+        synchronized static public void Broadcast(Socket from, String str) throws Exception {
+            for(Socket s : allSockets) {
+                if(s != from && !s.isClosed()) {
+                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                    out.write(str);
+                    out.newLine();
+                    out.flush();
+                }
+            }
+        }
     }
 
-	static void Client(String host) throws Exception
-	{
-		System.out.println("connecting to " + host);
-		Socket s = new Socket(host, port);
-		
-		new Thread(new EchoIncoming(getReader(s.getInputStream()))).start();
-		
-		BufferedReader in = getReader(System.in);
-		BufferedWriter out = getWriter(s.getOutputStream());
-		
-		for(;;)
-			writeString(in.readLine(), out);
-	}
-	
-	static class Broadcast implements Runnable
-	{
-		Socket client;
-		
-		public Broadcast(Socket c) { client = c; }
-		
-		public void run()
-		{
-			try
-			{
-				BufferedReader in = getReader(client.getInputStream());
-				
-				for(;;)
-				{
-					String msg = in.readLine();	// get a new message from the client
-					AllSockets.Broadcast(client, msg);
- 				}
-			}
-			catch(Exception e) 
-			{
-			}
-		}
-	}
-		
-	static void Server() throws Exception
-	{
-		ServerSocket s = new ServerSocket(port);
-		
-		for(;;)
-		{
-			System.out.println("listening");
-			Socket client = s.accept();
-			AllSockets.Add(client);
-			new Thread(new Broadcast(client)).start();
-		}
-	}
-		
-	// try running several clients and one server:
-	
-	// java SocketChat server 
-	// java SocketChat client 127.0.0.1 
-	// java SocketChat client 127.0.0.1 
-	// java SocketChat client 127.0.0.1 
+    static class BroadcastTask implements Runnable {
+        Socket client;
+        public BroadcastTask(Socket c) { client = c; }
+        public void run() {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+                String msg;
+                while ((msg = in.readLine()) != null) {
+                    AllSockets.Broadcast(client, msg);
+                }
+            } catch (Exception e) {
+            } finally {
+                AllSockets.Remove(client);
+            }
+        }
+    }
 
-	public static void main(String args[])
-	{
-		try
-		{
-			if(args[0].equals("client"))
-				Client(args[1]);
-			else
-				Server();
-		}		
-		catch(Exception e)
-		{
-			System.out.println("Connection error!");
-		}
-	}
+    static void Server() throws Exception {
+        ServerSocket s = new ServerSocket(port);
+        System.out.println("Server started...");
+        while(true) {
+            Socket client = s.accept();
+            AllSockets.Add(client);
+            new Thread(new BroadcastTask(client)).start();
+        }
+    }
+
+    static void Client(String host, String nickname) throws Exception {
+        Socket s = new Socket(host, port);
+        
+        new Thread(() -> {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) System.out.println(line);
+            } catch (IOException e) { }
+        }).start();
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()))) {
+            while (true) {
+                String msg = in.readLine();
+                if (msg == null || msg.equals("*quit")) {
+                    break;
+                }                out.write(nickname + "> " + msg);
+                out.newLine();
+                out.flush();
+            }
+        } finally {
+            s.close();
+            System.exit(0);
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            if(args.length > 0 && args[0].equals("client")) {
+                String host = args[1];
+                String nickname = (args.length > 2) ? args[2] : "Guest";
+                Client(host, nickname);
+            } else {
+                Server();
+            }
+        } catch(Exception e) {
+            System.out.println("Usage: java ImprovedChat client <IP> <Nickname> OR java ImprovedChat server");
+        }
+    }
 }
